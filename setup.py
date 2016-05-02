7cec56bd1ea0dd17d87208f34377251b2ef82064
randomdude999 <just.so.you.can.email.me@gmail.com>
2016-05-02 14:54:22 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='rule_n',
+        version='0.1',
+        py_modules=['rule_n'],
+        url='https://github.com/randomdude999/rule_n',
+        license='GPLv3',
+        author='randomdude999',
+        author_email='just.so.you.can.email.me@gmail.com',
+        description='Python Rule 110 (and 30, 90, 184) implementation',
+        long_description=open("README.md").read()
+)
