cb530fecbfca6d0277a1b119680f86414e1cf440
randomdude999 <just.so.you.can.email.me@gmail.com>
2016-05-06 21:51:17 +0300

    [ci skip] Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,16 @@ setup(
         version='0.1',
         py_modules=['rule_n'],
         url='https://github.com/randomdude999/rule_n',
-        license='GPLv3',
         author='randomdude999',
         author_email='just.so.you.can.email.me@gmail.com',
         description='Python Rule 110 (and 30, 90, 184) implementation',
-        long_description=open("README.rst").read()
+        long_description=open("README.rst").read(),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering :: Mathematics"
+        ]
 )
