6332e817b5458ce1529c96486dd284b94aacdf89
randomdude999 <just.so.you.can.email.me@gmail.com>
2016-05-15 13:46:55 +0300

    Add python 2 to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
             "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
             "Operating System :: OS Independent",
             "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
             "Programming Language :: Python :: 2.6",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
